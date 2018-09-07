f01ad14d56beafb616847788bf825d626a93a043
Matthew Lalmansingh <gpjazzplayer@gmail.com>
2018-09-06 20:33:06 -0500

    Add Landing Screen
    - Add Landing screen
    - Add Header and Sub-Header and center text
    - Add onTap listener
    - Style Headers

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -5,6 +5,16 @@ class LandingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Material(
       color: Colors.greenAccent,
+      child: new InkWell(
+        onTap: () => print("We tapped the screen!"),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text("Let's Quizzz", style: new TextStyle(color: Colors.white, fontSize: 50.0, fontWeight: FontWeight.bold),),
+            new Text("Tap to start!", style: new TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),),
+          ],
+        ),
+      ),
     );
   }
 }
