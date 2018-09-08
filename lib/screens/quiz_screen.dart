46a71118589bc54c32cc6859f8bf1123eb48986e
Matthew Lalmansingh <gpjazzplayer@gmail.com>
2018-09-08 13:56:23 -0500

    Add Question Text UI component
    - Add Question Text UI module
    - Display test question

--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -4,6 +4,7 @@ import '../utils/question.dart';
 import '../utils/quiz.dart';
 
 import '../ui/answer_button.dart';
+import '../ui/question_text.dart';
 
 class QuizScreen extends StatefulWidget {
   @override
@@ -19,6 +20,7 @@ class QuizScreenState extends State<QuizScreen> {
           // This is our main screen
           children: <Widget>[
             new AnswerButton(true, () => print("You answered true")),
+            new QuestionText(),
             new AnswerButton(false, () => print("You answered false"))
           ],
         ),
